98d9bb6f47e578e85f38db7964a4c59eb715e574
caozhiye <caozhiye@111.com.cn>
2018-02-26 15:10:05 +0800

    refine, remove hardcoded path.

--- dl_pic_111.py
+++ dl_pic_111.py
@@ -1,27 +1,37 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+# @Time    : 2018/2/01 12:09
+# @Author  : caozhiye
+
 import urllib.request
 from PIL import Image
 import matplotlib.pyplot as plt
 import shutil
-import time
+import os
+
+absolute_path = os.path.abspath(os.path.join(os.path.dirname(__file__), "./tmp.jpg"))
 
 
 def download_pic():
+    """
+    下载验证码并保存成文件
+    :return:
+    """
     url = 'http://passport.111.com.cn/sso/getSecurityCode.action'
 
     f = urllib.request.urlopen(url)
     data = f.read()
-    with open("d:/captcha/tmp.jpg", "wb") as code:
+    with open(absolute_path, "wb") as code:
         code.write(data)
 
     plt.ion()
-    img = Image.open("d:/captcha/tmp.jpg")
+    img = Image.open(absolute_path)
     plt.imshow(img)
     plt.pause(1)
 
     name = input('File name：')
     file_name = "./image_111/" + name + ".jpg"
     shutil.move("tmp.jpg", file_name)
-
     plt.close()
 
 
--- dl_pic_yaoex.py
+++ dl_pic_yaoex.py
@@ -7,6 +7,9 @@ import urllib.request
 from PIL import Image
 import matplotlib.pyplot as plt
 import shutil
+import os
+
+absolute_path = os.path.abspath(os.path.join(os.path.dirname(__file__), "./tmp.jpg"))
 
 
 def download_pic():
@@ -18,16 +21,16 @@ def download_pic():
 
     f = urllib.request.urlopen(url)
     data = f.read()
-    with open("d:/captcha/tmp.jpg", "wb") as code:
+    with open(absolute_path, "wb") as code:
         code.write(data)
 
     plt.ion()
-    img = Image.open("d:/captcha/tmp.jpg")
+    img = Image.open(absolute_path)
     plt.imshow(img)
     plt.pause(1)
 
     name = input('File name：')
-    file_name = "./image/" + name + ".jpg"
+    file_name = "./image_yaoex/" + name + ".jpg"
     shutil.move("tmp.jpg", file_name)
     plt.close()
 
